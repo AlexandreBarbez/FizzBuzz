50d36a5f07d3f2f1ab1f7d14382546d3f7eb4ad3
Halx <alexandre.barbez@gmail.com>
2018-05-22 22:16:33 +0200

    Deleting unused stuffs, learning IDEA shortcuts :)

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,13 +1,7 @@
 import static org.junit.Assert.*;
-import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
-
-    @Before
-    public void setUp() throws Exception {
-
-    }
 
     @Test
     public void fizzTest(){
